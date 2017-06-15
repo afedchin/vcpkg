02f3af8bf3607e5565dd776bfe5fbb0d4c3a3842
Anton Fedchin <anightik@gmail.com>
2017-06-15 10:09:42 +0300

    [pcre] fix for uwp

--- ports/pcre/portfile.cmake
+++ ports/pcre/portfile.cmake
@@ -20,16 +20,21 @@ vcpkg_extract_source_archive(${ARCHIVE})
 vcpkg_apply_patches(SOURCE_PATH ${SOURCE_PATH}
     PATCHES ${CMAKE_CURRENT_LIST_DIR}/fix-option.patch
             ${CMAKE_CURRENT_LIST_DIR}/fix-option-2.patch
-            ${CMAKE_CURRENT_LIST_DIR}/fix-arm-config-define.patch)
+            ${CMAKE_CURRENT_LIST_DIR}/fix-arm-config-define.patch
+            ${CMAKE_CURRENT_LIST_DIR}/fix-linkage-to-kernel32.patch
+            ${CMAKE_CURRENT_LIST_DIR}/fix-pdbs.patch
+            )
 
+set(VCPKG_LIBRARY_LINKAGE static)
 vcpkg_configure_cmake(
     SOURCE_PATH ${SOURCE_PATH}
-    PREFER_NINJA
+    #PREFER_NINJA
     OPTIONS -DPCRE_BUILD_TESTS=NO
             -DPCRE_BUILD_PCREGREP=NO
-            -DPCRE_BUILD_PCRE32=YES
-            -DPCRE_BUILD_PCRE16=YES
-            -DPCRE_BUILD_PCRE8=YES
+            -DINSTALL_DOC:BOOL=OFF
+            -DINSTALL_MSVC_PDB=ON
+            -DPCRE_MATCH_LIMIT_RECURSION=1500
+            -DPCRE_NEWLINE=ANYCRLF
             -DPCRE_SUPPORT_JIT=YES
             -DPCRE_SUPPORT_UTF=YES
             -DPCRE_SUPPORT_UNICODE_PROPERTIES=YES
@@ -38,22 +43,11 @@ vcpkg_configure_cmake(
             -DPCRE_SUPPORT_LIBZ=OFF
             -DPCRE_SUPPORT_LIBEDIT=OFF
             -DPCRE_SUPPORT_LIBREADLINE=OFF
-    # OPTIONS -DUSE_THIS_IN_ALL_BUILDS=1 -DUSE_THIS_TOO=2
-    # OPTIONS_RELEASE -DOPTIMIZE=1
-    # OPTIONS_DEBUG -DDEBUGGABLE=1
 )
 
-vcpkg_install_cmake()
+vcpkg_install_cmake(RELEASE_CONFIG RelWithDebInfo)
 
-foreach(FILE ${CURRENT_PACKAGES_DIR}/include/pcre.h ${CURRENT_PACKAGES_DIR}/include/pcreposix.h)
-    file(READ ${FILE} PCRE_H)
-    if(VCPKG_LIBRARY_LINKAGE STREQUAL "static")
-        string(REPLACE "defined(PCRE_STATIC)" "1" PCRE_H "${PCRE_H}")
-    else()
-        string(REPLACE "defined(PCRE_STATIC)" "0" PCRE_H "${PCRE_H}")
-    endif()
-    file(WRITE ${FILE} "${PCRE_H}")
-endforeach()
+
 
 file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug/include)
 file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug/share)
