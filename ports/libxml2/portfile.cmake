50d921acd847133d157630bf002286d491af1030
Anton Fedchin <anightik@gmail.com>
2017-06-17 12:50:15 +0300

    [libxm2] fix deprecated api

--- ports/libxml2/portfile.cmake
+++ ports/libxml2/portfile.cmake
@@ -18,6 +18,7 @@ vcpkg_extract_source_archive(${ARCHIVE})
 vcpkg_apply_patches(
     SOURCE_PATH ${SOURCE_PATH}
     PATCHES ${CMAKE_CURRENT_LIST_DIR}/0001-fix-uwp.patch
+            ${CMAKE_CURRENT_LIST_DIR}/0002-fix-deprecated-api.patch
 )
 
 file(COPY ${CMAKE_CURRENT_LIST_DIR}/CMakeLists.txt DESTINATION ${SOURCE_PATH})
